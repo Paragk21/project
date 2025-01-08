f9d4b8de499656750f33b8d23a27fb7e000e4dd1
Paragk21 <lakumparag@gmail.com>
2025-01-08 16:43:48 +0530

    logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ return (
             <div className="flex justify-between item-center">
                 <div className="flex items-center flex-shrink-0">
                     <img className='h-10 w-10 mr-2' src={logo} alt="logo" />
-                    <span className="text-xl tracking-tight">VirtualR</span>
+                    <span className="text-xl tracking-tight"></span>
                 </div>
                 {/* Desktop Nav */}
                 <ul className='hidden lg:flex ml-14 space-x-12' >
